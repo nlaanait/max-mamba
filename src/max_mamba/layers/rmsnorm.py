bb33aa0e0d099a60306078f02eb4dcec47d9a8b0
Numan Laanait <nlaanait@gmail.com>
2025-03-23 13:25:03 -0400

    refactor to use Weight placeholder instead of constant

--- src/max_mamba/layers/rmsnorm.py
+++ src/max_mamba/layers/rmsnorm.py
@@ -2,10 +2,7 @@ from dataclasses import dataclass
 from typing import Optional
 import numpy as np
 from max.dtype import DType
-from max.graph import (
-    TensorValue,
-    ops,
-)
+from max.graph import TensorValue, ops, Weight
 
 from max.nn import Module
 
@@ -15,15 +12,17 @@ class RMSNormGated(Module):
         self,
         hidden_size: tuple[int, ...],
         eps: float = 1e-6,
+        dtype: DType = DType.bfloat16,
     ):
         super().__init__()
         self.hidden_size = hidden_size
         self.eps = eps
+        self.dtype = dtype
+        self.weight = Weight("weight", self.dtype, self.hidden_size)
 
     def __call__(  # type: ignore
         self, h: TensorValue, gate: Optional[TensorValue] = None
     ) -> TensorValue:
-        self.weight = ops.constant(np.ones_like(h), dtype=DType.float32)
         weight: TensorValue = ops.cast(self.weight, h.dtype)
         if h.device:
             weight = weight.to(h.device)
--- tests/test_rmsnorm_gated.py
+++ tests/test_rmsnorm_gated.py
@@ -1,9 +1,6 @@
-from typing import Any
 from max import engine
 from max.dtype import DType
-from max.graph import Graph, TensorType, ops
-from max.graph.weights import load_weights
-from max.driver import Tensor
+from max.graph import Graph, TensorType
 from max_mamba.layers import RMSNormGated
 import numpy as np
 
@@ -13,7 +10,7 @@ import torch
 torch.manual_seed(1234)
 
 
-def init_hidden_state(hidden_size: tuple[int] = (8, 16)) -> torch.Tensor:
+def init_hidden_state(hidden_size: tuple[int, ...] = (8, 16)) -> torch.Tensor:
     return torch.rand(hidden_size)
 
 
@@ -22,19 +19,13 @@ def rmsnorm_gated_test():
     hidden_state = init_hidden_state(hidden_size=hidden_size)
     pt_rms_gated = MambaRMSNormGated(hidden_size=hidden_size)
     pt_output = pt_rms_gated.forward(hidden_state).detach().numpy()
-    print(f"torch output: {pt_output}")
-    max_output = max_rms_gated(hidden_state=hidden_state)
-    print(f"max output: {max_output}")
+    max_output = max_rms_gated(hidden_state=hidden_state.numpy())
     assert np.allclose(pt_output, max_output)
+    print("Passed Test")
 
 
 def max_rms_gated(hidden_state: np.ndarray) -> np.ndarray:
-    # 1. Build the graph
     hidden_size = tuple(hidden_state.shape)
-    # weights = np.ones_like(hidden_state, dtype=hidden_state.dtype)
-    # weights = Tensor.from_numpy(weights)
-    # input_type = TensorType(dtype=DType.float32, shape=hidden_size)
-    print(hidden_size)
     max_rms_gated = Graph(
         "rmsnorm_gated",
         RMSNormGated(hidden_size=hidden_size),
@@ -42,14 +33,10 @@ def max_rms_gated(hidden_state: np.ndarray) -> np.ndarray:
     )
 
     session = engine.InferenceSession()
-    model = session.load(max_rms_gated)
-
-    for tensor in model.input_metadata:
-        print(f"name: {tensor.name}, shape: {tensor.shape}, dtype: {tensor.dtype}")
-
-    # 3. Execute the graph
+    model = session.load(
+        max_rms_gated, weights_registry={"weight": np.ones_like(hidden_state)}
+    )
     ret = model.execute(hidden_state)[0]
-    print("result:", ret)
     ret_max = ret.to_numpy()
     return ret_max
 
