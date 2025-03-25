a98000e1532a207f2072aa94e93bcb93372d1685
Numan Laanait <nlaanait@gmail.com>
2025-03-25 11:18:24 -0400

    implement mamba2cache and tests

---
+++ src/max_mamba/layers/cache.py
@@ -0,0 +1,119 @@
+from max_mamba import Mamba2Config
+from max.dtype import DType
+from max.graph import ops, Weight, TensorValue
+import numpy as np
+
+
+class Mamba2Cache:
+    """
+    Arguments:
+        config: Mamba2Config
+        batch_size: int
+
+    Attributes:
+        dtype: (`torch.dtype`):
+            The default `dtype` used to initializing the cache.
+        conv_kernel_size: (`int`):
+            Model's convolution kernel size taken from config.
+        n_groups: (`int`):
+            Model's number of groups taken from the config - similar to tensor parallel in Transformer.
+        state_size: (`int`):
+            Model's SSM state size taken from config.
+        num_heads: (`int`):
+            The number of heads used in the linear attention / SSM.
+        head_dim: (`int`):
+            The respective dimension of the heads used in the linear attention / SSM.
+        intermediate_size: (`int`):
+            Model's intermediate_size based on (expand * hidden_dim) from config.
+        conv_states: (`TensorValue`):
+            A tensor of shape `[num_layers, batch_size, conv_kernel_size, intermediate_size + 2 * n_groups * state_size]` that holds convolutional states.
+        ssm_states: (`TensorValue`):
+            A tensor of shape `[num_layers, batch_size, num_heads, head_dim, state_size]` that holds ssm states.
+    """
+
+    def __init__(
+        self,
+        config: Mamba2Config,
+        batch_size: int,
+    ):
+        self.dtype = Mamba2Config.dtype
+        self.conv_kernel_size = config.conv_kernel
+        self.n_groups = config.n_groups
+        self.state_size = config.state_size
+        self.num_heads = config.num_heads
+        self.head_dim = config.head_dim
+        self.intermediate_size = int(config.expand * config.hidden_size)
+        self.conv_states = Weight(
+            "conv_states",
+            self.dtype,
+            (
+                config.num_hidden_layers,
+                batch_size,
+                self.intermediate_size + 2 * self.n_groups * self.state_size,
+                self.conv_kernel_size,
+            ),
+        )
+        self.ssm_states = Weight(
+            "ssm_states",
+            self.dtype,
+            (
+                config.num_hidden_layers,
+                batch_size,
+                self.num_heads,
+                self.head_dim,
+                self.state_size,
+            ),
+        )
+        self.conv_mask = np.zeros(
+            (
+                config.num_hidden_layers,
+                batch_size,
+                self.intermediate_size + 2 * self.n_groups * self.state_size,
+                self.conv_kernel_size,
+            ),
+            dtype=np.int8,
+        )
+        self.ssm_mask = np.zeros(
+            (
+                config.num_hidden_layers,
+                batch_size,
+                self.num_heads,
+                self.head_dim,
+                self.state_size,
+            ),
+        )
+
+    def update_conv_state(
+        self, layer_idx: int, new_conv_state: TensorValue, cache_init: bool = False
+    ) -> TensorValue:
+        self.conv_mask[layer_idx, ...] = 1
+        self.conv_states = ops.cast(self.conv_states, new_conv_state.dtype)
+        if new_conv_state.device:
+            self.conv_states: TensorValue = self.conv_states.to(new_conv_state.device)
+        if cache_init:
+            mask = ops.constant(self.conv_mask, DType.bool)
+            self.conv_states = ops.masked_scatter(
+                self.conv_states, mask, new_conv_state
+            )
+        else:
+            # NEED To implement tensor roll
+            # self.conv_states[layer_idx][:, :, -1] = new_conv_state[:, 0, :]
+            return new_conv_state
+        self.conv_mask[layer_idx, ...] = 0
+        return self.conv_states[layer_idx]
+
+    def update_ssm_state(
+        self, layer_idx: int, new_ssm_state: TensorValue
+    ) -> TensorValue:
+        self.ssm_mask[layer_idx, ...] = 1
+        self.ssm_states = ops.cast(self.ssm_states, new_ssm_state.dtype)
+        if new_ssm_state.device:
+            self.ssm_states: TensorValue = self.ssm_states.to(new_ssm_state.device)
+        mask = ops.constant(self.ssm_mask, DType.bool)
+        self.ssm_states = ops.masked_scatter(self.ssm_states, mask, new_ssm_state)
+        self.ssm_mask[layer_idx, ...] = 0
+        return self.ssm_states[layer_idx]
+
+    def reset(self):
+        self.conv_states = ops.mul(self.conv_states, 0)
+        self.ssm_states = ops.mul(self.ssm_states, 0)
---
+++ tests/test_cache.py
@@ -0,0 +1,164 @@
+from max.engine.api import InferenceSession
+from max.dtype import DType
+from max.graph import Graph, TensorType
+from max_mamba.layers import Mamba2Cache
+from max_mamba.config import Mamba2Config
+import numpy as np
+
+
+from transformers.models.mamba2.modeling_mamba2 import Mamba2Cache as HF_Mamba2Cache
+from transformers import Mamba2Config as HF_MAMBA2CFG
+
+import torch
+
+torch.manual_seed(1234)
+
+
+def init_pt_tensor(
+    size: tuple[int, ...] = (8, 16), dtype: torch.dtype = torch.float32
+) -> torch.Tensor:
+    return torch.rand(size=size, dtype=dtype)
+
+
+def init_np_tensor(size: tuple[int, ...], dtype: np.dtype) -> np.ndarray:
+    return np.random.rand(*size).astype(dtype)
+
+
+def init_mamba2configs() -> tuple[Mamba2Config, HF_MAMBA2CFG]:
+    max_config = Mamba2Config()
+    hf_config = HF_MAMBA2CFG()
+    return (max_config, hf_config)
+
+
+def hf_cache(
+    batch_size: int,
+    config: HF_MAMBA2CFG,
+    new_conv_state: torch.Tensor,
+    new_ssm_state: torch.Tensor,
+    layer_idx: int,
+    dtype: torch.dtype = torch.float32,
+) -> tuple[np.ndarray, ...]:
+    pt_cache = HF_Mamba2Cache(config, batch_size=batch_size, dtype=dtype)
+    pt_cache_state_pre_init = pt_cache.update_conv_state(
+        layer_idx=layer_idx, new_conv_state=new_conv_state, cache_init=True
+    )
+    # TODO: re-enable after resolving the assigment issue in Transformers
+    # pt_cache_state_post_init = pt_cache.update_conv_state(
+    #     layer_idx=layer_idx, new_conv_state=pt_cache_state_pre_init, cache_init=False
+    # )
+    pt_cache_ssm_state = pt_cache.update_ssm_state(
+        layer_idx=layer_idx, new_ssm_state=new_ssm_state
+    )
+
+    return (
+        pt_cache_state_pre_init.numpy(),
+        pt_cache_state_pre_init.numpy(),
+        pt_cache_ssm_state.numpy(),
+    )
+
+
+def test_cache():
+    hf_config = HF_MAMBA2CFG()
+    max_config = Mamba2Config.generate(
+        pipeline_config=None,
+        huggingface_config=hf_config,
+        dtype=DType.float32,
+        logits_postprocessor=None,
+    )
+    batch_size = 1
+    conv_state_size = (
+        batch_size,
+        int(hf_config.expand * hf_config.hidden_size)
+        + 2 * hf_config.n_groups * hf_config.state_size,
+        hf_config.conv_kernel,
+    )
+    ssm_state_size = (
+        batch_size,
+        hf_config.num_heads,
+        hf_config.head_dim,
+        hf_config.state_size,
+    )
+    layer_idx = 1
+    conv_state = init_np_tensor(size=conv_state_size, dtype=np.float32)
+    ssm_state = init_np_tensor(size=ssm_state_size, dtype=np.float32)
+    pt_conv_state = torch.from_numpy(conv_state)
+    pt_ssm_state = torch.from_numpy(ssm_state)
+    pt_cache_state_pre_init, pt_cache_state_post_init, pt_cache_ssm_state = hf_cache(
+        batch_size=batch_size,
+        config=hf_config,
+        new_conv_state=pt_conv_state,
+        new_ssm_state=pt_ssm_state,
+        layer_idx=layer_idx,
+    )
+    max_cache_state_pre_init, max_cache_state_post_init, max_cache_ssm_state = (
+        max_cache(
+            batch_size=batch_size,
+            config=max_config,
+            new_conv_state=conv_state,
+            new_ssm_state=ssm_state,
+            layer_idx=layer_idx,
+        )
+    )
+    assert np.allclose(pt_cache_state_pre_init, max_cache_state_pre_init)
+    assert np.allclose(pt_cache_state_post_init, max_cache_state_post_init)
+    assert np.allclose(pt_cache_ssm_state, max_cache_ssm_state)
+
+
+def max_cache(
+    batch_size: int,
+    config: Mamba2Config,
+    new_conv_state: np.ndarray,
+    new_ssm_state: np.ndarray,
+    layer_idx: int,
+):
+    input_size = new_conv_state.shape
+    conv_cache_size = (config.num_hidden_layers,) + input_size
+    ssm_cache_size = (
+        config.num_hidden_layers,
+        batch_size,
+        config.num_heads,
+        config.head_dim,
+        config.state_size,
+    )
+    init_conv_states = np.zeros(conv_cache_size, dtype=np.float32)
+    init_ssm_states = np.zeros(ssm_cache_size, dtype=np.float32)
+    with Graph(
+        "mamba2_cache",
+        input_types=(
+            TensorType(DType.float32, new_conv_state.shape),
+            TensorType(DType.float32, new_ssm_state.shape),
+        ),
+    ) as cache_graph:
+        conv_state, ssm_state = cache_graph.inputs
+        cache = Mamba2Cache(config=config, batch_size=batch_size)
+        conv_state_pre_init = cache.update_conv_state(
+            layer_idx=layer_idx, new_conv_state=conv_state, cache_init=True
+        )
+        conv_state_post_init = cache.update_conv_state(
+            layer_idx=layer_idx, new_conv_state=conv_state, cache_init=False
+        )
+        ssm_state = cache.update_ssm_state(layer_idx=layer_idx, new_ssm_state=ssm_state)
+        cache.reset()
+        cache_graph.output(conv_state_pre_init, conv_state_post_init, ssm_state)
+
+    sess = InferenceSession()
+    cache = sess.load(
+        cache_graph,
+        weights_registry={
+            "conv_states": init_conv_states,
+            "ssm_states": init_ssm_states,
+        },
+    )
+
+    conv_state_pre_init, conv_state_post_init, ssm_state = cache.execute(
+        new_conv_state.astype(np.float32), new_ssm_state.astype(np.float32)
+    )
+    return (
+        conv_state_pre_init.to_numpy(),
+        conv_state_pre_init.to_numpy(),
+        ssm_state.to_numpy(),
+    )
+
+
+if __name__ == "__main__":
+    test_cache()
